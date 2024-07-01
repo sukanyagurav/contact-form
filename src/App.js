a96e9d787a3d3e06cd13f009ffad5f96ffac8d6a
sukanya gurav <sukanyagurav6@gmail.com>
2024-07-01 21:40:15 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import {z} from "zod";
 import { useForm } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod"
+import { useEffect, useState } from "react";
+import succeses from './assets/images/icon-success-check.svg'
+
+function ShowToast(){
+  return <div className="toast">
+    <div className="toast__header flex-row">
+      <img src={succeses}  />
+      <h2>Messge Sent!</h2>
+    </div>
+    <div className="toast__body">
+      <p>Thanks for completing the form. We'll be in touch soon!</p>
+    </div>
+  </div>
+}
+
 function App() {
   const schema = z.object({
     firstName:z.string().min(2,{ message:'First Name should have at least 2 characters'}),
@@ -12,14 +27,18 @@ function App() {
     }),
     type: z.string({message:'Please select a query type!'}).refine(val=> ['general_enquiry','support_request'].includes(val)),
   })
+  const [sent,setSent] = useState(false)
+  const {register,handleSubmit,formState:{errors,isValid},reset} = useForm({resolver:zodResolver(schema)})
 
-  const {register,handleSubmit,formState:{errors}} = useForm({resolver:zodResolver(schema)})
   
-  function submitData(data){
-    console.log(data)
+  function submitData(){
+    
+    setSent(true)
+    reset()
   }
   return (
     <main>
+    {sent  && <ShowToast/>}
       <h1>Contact Us</h1>
       <form onSubmit={handleSubmit(submitData)}>
         <div className="form__fields">
@@ -28,13 +47,13 @@ function App() {
               <label htmlFor="firstName">First Name <sup>*</sup></label>
               <input type="text" id="firstName" 
               {...register("firstName",{required:true})}/>
-              {errors.firstName && <span >{errors.firstName.message}</span>}
+              {errors.firstName && <span className="error" >{errors.firstName.message}</span>}
             </div>
 
             <div className="form__field">
               <label htmlFor="lastName">Last Name <sup>*</sup></label>
               <input type="text" id="lastName"  {...register("lastName")} />
-              {errors.lastName && <span >{errors.lastName.message}</span>}
+              {errors.lastName && <span   className="error">{errors.lastName.message}</span>}
 
             </div>
         </div>
@@ -43,32 +62,44 @@ function App() {
 
           <label htmlFor="email">Email Address <sup>*</sup></label>
           <input type="email" id="email"  {...register("email")} />
-          {errors.email && <span >{errors.email.message}</span>}
+          {errors.email && <span   className="error">{errors.email.message}</span>}
         </div>
 
-        <div className="form__field">
-          <label >Query Type <sup>*</sup></label>
-          <input type="radio" name="type"  value="general_enquiry"
-           id="general_enquiry" {...register("type")} />
-          <label htmlFor="general_enquiry">General Enquiry</label>
-          <input type="radio" name="type" id="support_request"
-           value="support_request" {...register("type")} />
-          <label htmlFor="support_request">Support Request</label>
+        <div className="query_type">
+          <span className="title">Query Type <sup>*</sup></span>
+          <div className="form__fields">
+            <div className="form__field flex-row">
+               <input type="radio" name="type"  value="general_enquiry"
+                id="general_enquiry" {...register("type")} />
+                <span className="custom__radio"></span>
+              <label htmlFor="general_enquiry">General Enquiry</label>
+            </div>
+            <div className="form__field flex-row">
+                <input type="radio" name="type" id="support_request"
+                  value="support_request" {...register("type")} />
+                   <span className="custom__radio"></span>
+                  <label htmlFor="support_request">Support Request</label>
+            </div>
+          </div>
           {errors.type && (
-            <span>{errors.type.message}</span>
+            <span  className="error">{errors.type.message}</span>
           )}
         </div>
         <div className="form__field">
           <label htmlFor="message">Message <sup>*</sup></label>
-          <textarea id="message"  {...register("message")}></textarea>
-          {errors.message && <span >{errors.message.message}</span>}
+          <textarea id="message"  {...register("message")}/>
+          {errors.message && <span   className="error">{errors.message.message}</span>}
         </div>
-        <div className="form__field">
-          <input type="checkbox"  id="consent"  {...register("consent")}/>
+        <div className="term">
+        <div className="flex-row">
+        <input type="checkbox"  id="consent"  {...register("consent")}/>
+          <span className="custom__checkbox"></span>
           <label htmlFor="consent">I consent to being contacted by the team <sup>*</sup></label>
-          {errors.consent && <span >{errors.consent.message}</span>}
         </div>
-        <button>Submit</button>
+         
+          {errors.consent && <span  className="error" >{errors.consent.message}</span>}
+        </div>
+        <button className="btn">Submit</button>
       </form>
     </main>
   );
